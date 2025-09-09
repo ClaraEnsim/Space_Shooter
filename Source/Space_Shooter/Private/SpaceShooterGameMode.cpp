ab72d2892acd933bc28c53f19b106e79adeee389
CLARA TOURNAY <ctournay@etu.uqac.ca>
2025-09-09 15:47:11 -0400

    init gameplay

---
+++ Source/Space_Shooter/Private/SpaceShooterGameMode.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Space_Shooter/Public/SpaceShooterGameMode.h"
+#include "Kismet/GameplayStatics.h"
+
+ASpaceShooterGameMode::ASpaceShooterGameMode()
+{
+	MaxScore = 10;
+	InitialLives = 3;
+	CurrentScore = 0;
+	CurrentLives = InitialLives;
+}
+
+void ASpaceShooterGameMode::AddPoint()
+{
+	CurrentScore++;
+
+	if (CurrentScore >= MaxScore)
+	{
+		CheckGameOver();
+	}
+}
+
+void ASpaceShooterGameMode::LoseLife()
+{
+	CurrentLives--;
+
+	if (CurrentLives <= 0)
+	{
+		CheckGameOver();
+	}
+}
+
+void ASpaceShooterGameMode::CheckGameOver()
+{
+	if (CurrentScore >= MaxScore)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Victoire ! Score atteint."));
+		// TODO : déclencher un écran de victoire via Blueprint ou HUD
+	}
+	else if (CurrentLives <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Game Over ! Plus de vies."));
+		// TODO : déclencher un écran de défaite via Blueprint ou HUD
+	}
+}
+
+
---
+++ Source/Space_Shooter/Public/SpaceShooterGameMode.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceShooterGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACE_SHOOTER_API ASpaceShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASpaceShooterGameMode();
+
+protected:
+	// Score et vies du joueur
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+	int32 MaxScore;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay")
+	int32 InitialLives;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gameplay")
+	int32 CurrentScore;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gameplay")
+	int32 CurrentLives;
+
+public:
+	// Appelée quand une cible est détruite
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	void AddPoint();
+
+	// Appelée quand le joueur subit une collision
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	void LoseLife();
+
+	// Vérifie conditions de victoire ou défaite
+	UFUNCTION(BlueprintCallable, Category = "Gameplay")
+	void CheckGameOver();
+};
